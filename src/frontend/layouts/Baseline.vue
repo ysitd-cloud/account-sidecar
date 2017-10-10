6beef3154583eb2d372204b6b0b659455f0836cb
Tony Yip <tony@opensource.hk>
2017-10-10 14:06:29 +0800

    Add drawer

--- src/frontend/layouts/Baseline.vue
+++ src/frontend/layouts/Baseline.vue
@@ -1,7 +1,11 @@
 <template>
     <v-app>
+        <v-navigation-drawer app v-model="drawer"></v-navigation-drawer>
         <v-toolbar dark color="primary" app>
-            <v-toolbar-title>YSITD Cloud Account</v-toolbar-title>
+            <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>
+                <a href="/">YSITD Cloud Account</a>
+            </v-toolbar-title>
         </v-toolbar>
         <main>
             <v-content>
@@ -13,3 +17,19 @@
         <v-footer app>&copy; 2017 YSITD Cloud</v-footer>
     </v-app>
 </template>
+
+<style scoped lang="stylus">
+    .toolbar__title a
+        color: inherit
+        text-decoration: none
+</style>
+
+<script>
+    export default {
+      data() {
+        return {
+          drawer: false,
+        };
+      },
+    };
+</script>
