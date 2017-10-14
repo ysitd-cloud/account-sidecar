5549b9dffe92331740a59c95f99212dddb133701
Tony Yip <tony@opensource.hk>
2017-10-15 00:21:51 +0800

    Deep tracking with user ID

--- src/frontend/clientEntry.js
+++ src/frontend/clientEntry.js
@@ -6,16 +6,21 @@ const { app, router, store } = createApp();
 /* eslint-disable no-underscore-dangle */
 if (window.__INITIAL_STATE__) {
   store.replaceState(window.__INITIAL_STATE__);
+  if ('user' in window.__INITIAL_STATE__.params) {
+    window.gtag('set', { user_id: window.__INITIAL_STATE__.params.user.username });
+  }
 }
 /* eslint-enable */
 
+window.gtag('config', 'UA-103662489-1', {
+  send_page_view: false,
+  linker: {
+    domains: ['ysitd.cloud'],
+    accept_incoming: true,
+  },
+});
+
 router.onReady(() => {
   app.$mount('#app');
-  window.gtag('config', 'UA-103662489-1', {
-    send_page_view: false,
-    linker: {
-      domains: ['ysitd.cloud'],
-    },
-  });
 });
 
