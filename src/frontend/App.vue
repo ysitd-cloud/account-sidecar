3f66b19c2754a577c41d0023862d8f236001407a
Tony Yip <tony@opensource.hk>
2017-10-20 14:42:54 +0800

    Fix wrong icon link

--- src/frontend/App.vue
+++ src/frontend/App.vue
@@ -15,8 +15,8 @@
         link: [
           { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' },
           { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' },
-          { rel: 'icon', href: 'https://ysitc.cloud/logo-16x16.png', sizes: '16x16' },
-          { rel: 'icon', href: 'https://ysitc.cloud/logo-32x32  .png', sizes: '32x32' },
+          { rel: 'icon', href: 'https://ysitd.cloud/logo-16x16.png', sizes: '16x16' },
+          { rel: 'icon', href: 'https://ysitd.cloud/logo-32x32.png', sizes: '32x32' },
         ],
       },
     };
